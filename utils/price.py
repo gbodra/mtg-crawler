58f33f17581cbc5b6244062e935e610ce19dbc80
Gustavo Bodra <gustavobodra@yahoo.com.br>
2022-05-05 20:21:45 -0300

    Ajuste no agregador de precos para melhorar performance

--- utils/price.py
+++ utils/price.py
@@ -1,6 +1,5 @@
-import pprint
 from tqdm import tqdm
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 
 class Price:
@@ -10,44 +9,60 @@ class Price:
 
     def aggregate_prices(self):
         prices_collection = self.db.prices
-        price_cursor = prices_collection.find()
+        prices_aggregated_collection = self.db.prices_aggregated
+        price_list = list(prices_collection.find())
 
-        for price in tqdm(price_cursor):
-            price_history = []
+        for price in tqdm(price_list):
+            price.pop('_id', None)
+            price.pop('tcgplayer_id', None)
+
             for price_detail in price['prices']:
-                price_item = {
-                    'printingType': price_detail['printingType'],
-                    'marketPrice': price_detail['marketPrice'],
-                    'buylistMarketPrice': price_detail['buylistMarketPrice'],
-                    'listedMedianPrice': price_detail['listedMedianPrice'],
-                    'created_at': price['created_at']
-                }
-                price_history.append(price_item)
+                price_detail.update({'created_at': price['created_at']})
 
-            if price['id'] not in self.price_dict.keys():
-                self.price_dict[price['id']] = {'price_history': price_history}
-            else:
-                self.price_dict[price['id']]['price_history'].extend(price_history)
+            price.pop('created_at', None)
+            query = {"id": price['id']}
+
+            upsert_data = {
+                '$push': {'price_history': {'$each': price['prices']}},
+                '$setOnInsert': {'id': price['id']}
+            }
+
+            prices_aggregated_collection.update_one(query, upsert_data, upsert=True)
+
+        prices_collection.drop()
+
+    def load_prices(self):
+        today = datetime.now()
+        DD = timedelta(days=2)
+        earlier = today - DD
+        pipeline = [
+            {'$project': {
+                'price_history': {'$filter': {
+                    'input': '$price_history',
+                    'as': 'item',
+                    'cond': {'$and': [
+                        {'$eq': ['$$item.printingType', 'Normal']},
+                        {'$gt': ['$$item.created_at', earlier]}
+                    ]}
+                }}
+            }}
+        ]
+        prices_aggregated_collection = self.db.prices_aggregated
+        price_list = list(prices_aggregated_collection.aggregate(pipeline))
+        self.price_dict = {item['id']: {'price_history': item['price_history']} for item in price_list}
 
     def calculate_price_movements(self):
+        self.load_prices()
         for card_price in tqdm(self.price_dict.items()):
             list_normal_price = []
-            list_foil_price = []
             for price_history in card_price[1]['price_history']:
                 if price_history['printingType'] == 'Normal':
                     list_normal_price.append(price_history)
-                else:
-                    list_foil_price.append(price_history)
 
             normal_money, normal_percentage, normal_last_price = self._get_last_movement(list_normal_price)
             self.price_dict[card_price[0]]['normal_last_price'] = normal_last_price
             self.price_dict[card_price[0]]['normal_movement_money'] = normal_money
             self.price_dict[card_price[0]]['normal_movement_percentage'] = normal_percentage
-
-            foil_money, foil_percentage, foil_last_price = self._get_last_movement(list_foil_price)
-            self.price_dict[card_price[0]]['foil_last_price'] = foil_last_price
-            self.price_dict[card_price[0]]['foil_movement_money'] = foil_money
-            self.price_dict[card_price[0]]['foil_movement_percentage'] = foil_percentage
 
     def get_card_name(self, id):
         cards_collection = self.db.cards
@@ -64,7 +79,8 @@ class Price:
         }
 
         for item in self.price_dict.items():
-            if item[1]['normal_movement_percentage'] > THRESHOLD:
+            # Percentage has to be over the threshold but also the money movement has to be over 1 USD
+            if item[1]['normal_movement_percentage'] > THRESHOLD and item[1]['normal_movement_money'] > 1.0:
                 dict_alert['cards'].append({
                     'id': item[0],
                     'name': self.get_card_name(item[0]),
